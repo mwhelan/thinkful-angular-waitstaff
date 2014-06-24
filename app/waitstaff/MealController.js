9018fc7160b02fc2f2be34d4206c40f927627fa7
Michael Whelan <mjmwdev@gmail.com>
2014-06-24 15:25:59 +0100

    created initial static page

--- app/waitstaff/MealController.js
+++ app/waitstaff/MealController.js
@@ -1,8 +1,16 @@
-﻿(function () {
+﻿(function() {
     'use strict';
 
-    angular.module('app').controller("MealController", function($scope) {
-        var vm = this;
+    angular.module('app')
+        .controller("MealController", function() {
+            var vm = this;
+        this.meal = new Meal(0.0, 0.0, 0.0);
 
-    });
+        });
 })();
+
+function Meal(price, taxRate, tipPercentage) {
+    this.price = price;
+    this.taxRate = taxRate;
+    this.tipPercentage = tipPercentage;
+}
