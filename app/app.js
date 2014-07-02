50b536d5524c57b0ee49d973aff0e1188f2fa4b1
Michael Whelan <mjmwdev@gmail.com>
2014-07-02 10:40:47 +0100

    added font awesome and completed routes

--- app/app.js
+++ app/app.js
@@ -1,3 +1,28 @@
 ﻿'use strict';
 
-angular.module("app", []);
+angular.module("app", ["ngRoute"])
+    .value("totalEarnings", {
+        tipTotal: 0,
+        mealCount: 0,
+        averageTip: 0
+    })
+    .config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "app/home.html"
+        })
+        .when("/new-meal", {
+            templateUrl: "app/new-meal.html",
+            controller: "MealController"
+        })
+        .when("/my-earnings", {
+            templateUrl: "app/my-earnings.html",
+            controller: "EarningsController"
+        })
+        .otherwise({
+            redirectTo: "/"
+        })
+        .when('/error', {
+            template: '<p>Error Page Not Found</p>'
+        });
+});
